53b9aaada30c7c463c9352c7f44aa03d01dc2c13
SurajGupta2009 <219184096+SurajGupta2009@users.noreply.github.com>
2026-09-08 16:07:03 +0000

    capacitors: ch5 dielectrics (bound charge, Clausius-Mossotti, geometries, dielectrophoresis, Paschen, losses, ferroelectrics) + 10 questions
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- capacitors/tools/mathfix.py
+++ capacitors/tools/mathfix.py
@@ -10,8 +10,11 @@ import re, glob, sys
 changed=[]
 for f in glob.glob('*.html'):
     s=open(f, encoding='utf-8').read(); o=s
-    s=re.sub(r'<m(?=[(\\=!])', '<m>', s)
-    s=re.sub(r'<m (?=[A-Za-z\\])', '<m>', s)
+    s=re.sub(r'<m([A-Za-z])(?=</m>)', r'<m>\1', s)  # <mA</m> -> <m>A</m>
+    s=re.sub(r'<m(?=[^\sA-Za-z>/])', '<m>', s)      # <m\x, <m1, <m(, <m=, <m- …
+    s=re.sub(r'<m (?=[A-Za-z\\0-9])', '<m>', s)      # <m \kappa, <m Q_1 …
+    s=re.sub(r'<m>\s*\\([EDPF])(?![a-zA-Z])', r'<m>\\vec \1', s)   # \E -> \vec E etc.
+    s=re.sub(r'(<m>[^<]*?)\\([EDPF])(?![a-zA-Z])', lambda m: m.group(1)+r'\vec '+m.group(2), s)
     s=re.sub(r'<m>([^<]*?)</m>', lambda m:'<m>'+re.sub(r'\s*\n\s*',' ',m.group(1))+'</m>', s, flags=re.S)
     # unbalanced <m> on a line: report
     for i,l in enumerate(s.split('\n'),1):
